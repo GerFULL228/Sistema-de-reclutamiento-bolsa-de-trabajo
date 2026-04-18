19aa6616985f632c033d3b6dada45cebdf2acc34
Gerson <gersonmachuca24@gmail.com>
2026-04-18 00:08:18 -0500

    feat: configuracion inical del proyecto y properties para la conexion a la base de datos en la nube con supabase

---
+++ src/test/java/com/example/sistemadereclutamiento/SistemaDeReclutamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemadereclutamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReclutamientoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
